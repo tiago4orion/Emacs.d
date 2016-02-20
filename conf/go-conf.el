97aedfbe29e3fac866760f126707e908c2fed85f
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-02-20 21:53:29 -0200

    Add keybinding to format go code when in org-src-mode

--- conf/go-conf.el
+++ conf/go-conf.el
@@ -14,17 +14,18 @@
 (setq exec-path (cons (expand-file-name "~/projects/personal/go") exec-path))
 (add-to-list 'exec-path (expand-file-name "~/projects/go-workspace/src/github.com/nsf/gocode/bin"))
 
-(defun auto-complete-for-go ()
-  )
-(add-hook 'go-mode-hook 'auto-complete-for-go)
-
 (defun my-go-mode-hook ()
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
   ; Call Gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
-
-  (auto-complete-mode 1)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
   ; Godef jump key binding
-  (local-set-key (kbd "M-.") 'godef-jump))
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "C-c C-i") 'gofmt-before-save))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (with-eval-after-load 'go-mode
